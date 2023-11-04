c569d7a5a23cc8e58ca36b5963c91d72f98d586d
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-11-04 05:53:40 -0700

    style: apply the default font to elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Compass Card</h1>
+    <main className="font-sg flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Compass rights</h1>
     </main>
   )
 }
