7d9621ce87cadf000e1321568767a381f03b14dd
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-11-05 15:26:27 -0800

    feat: set link to the service status

--- client/src/components/home/sub-feature-card.tsx
+++ client/src/components/home/sub-feature-card.tsx
@@ -1,18 +1,27 @@
+'use client'
 import React, { cloneElement } from 'react'
+import { useRouter } from 'next/navigation'
 
 type Props = {
   title: string;
+  link: string;
   icon: JSX.Element;
   isEnabled: boolean;
 }
 
 export default function SubFeatureCard(props: Props) {
+
+  const router = useRouter();
+
   return (
-    <div className={`
+    <div
+      onClick={() => router.push(props.link)}
+      className={`
       flex flex-col justify-center items-center gap-y-1 grow py-3 
       bg-blue-light rounded-md text-4xl cursor-pointer
       ${!props.isEnabled && 'opacity-50'}
-    `}>
+    `}
+    >
       {cloneElement(props.icon, { className: 'text-blue-main' })}
       <p className='w-16 text-sm text-blue-main text-center font-bold'>{props.title}</p>
     </div>
