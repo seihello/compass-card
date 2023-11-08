f44ee1063188e1e9541c9678f7a706ef895aebb5
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-11-08 15:29:37 -0800

    style: set icons to the toggle button for the dark mode

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,7 +1,8 @@
 'use client'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState, cloneElement } from 'react'
 import { HeaderContext } from '@/context/header-context';
 import { useTheme } from 'next-themes';
+import { TbMoonStars, TbSun } from 'react-icons/tb';
 
 export default function Header() {
 
@@ -25,15 +26,23 @@ export default function Header() {
   return (
     <div
       className='container w-full max-w-none h-14 mb-1 flex justify-center items-center relative bg-white dark:bg-gray-0'
-      // style={{
-      //   boxShadow: title && '0px 0px 10px 0px #0000000A'
-      // }}
+    // style={{
+    //   boxShadow: title && '0px 0px 10px 0px #0000000A'
+    // }}
     >
       <h6 className='text-gray-0 text-center font-bold'>{title}</h6>
       <button
-        className='absolute top-1/2 right-0 transform -translate-x-full -translate-y-1/2 dark:text-gray-3'
-        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-      >{theme === 'light' ? 'Dark' : 'Light'}</button>
+        className='absolute top-1/2 right-0 transform -translate-x-full -translate-y-1/2 dark:text-gray-3'>
+      </button>
+
+
+      {cloneElement(theme === 'light' ? <TbMoonStars /> : <TbSun className=''/>,
+        {
+          onClick: () => setTheme(theme === 'dark' ? 'light' : 'dark'),
+          style: {fontSize: '1.5rem' },
+          className: `absolute top-1/2 right-0 transform -translate-x-full -translate-y-1/2 ${theme === 'dark' && 'text-gray-3'}`
+        })}
+
     </div>
   )
 }
