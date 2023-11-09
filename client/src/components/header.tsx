84255e146b3e3feb3a933b7cc161bcc7ab65fa07
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-11-08 16:15:31 -0800

    style: fix the problem of the header not showing when the page is loaded

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -19,10 +19,6 @@ export default function Header() {
     setTheme('light');
   }, [])
 
-  if (!mounted) {
-    return null;
-  }
-
   return (
     <header
       className='container w-full max-w-none h-14 mb-1 flex justify-center items-center bg-white dark:bg-gray-0'
@@ -30,13 +26,13 @@ export default function Header() {
     //   boxShadow: title && '0px 0px 10px 0px #0000000A'
     // }}
     >
-      <div className='relative w-full max-w-[768px] h-full'>
+      <div className='relative w-full max-w-[768px] h-full flex justify-center items-center'>
         <h6 className='text-gray-0 dark:text-gray-3 text-center font-bold'>{title}</h6>
         <button
           className='absolute top-1/2 right-0 transform -translate-x-full -translate-y-1/2 dark:text-gray-3'>
         </button>
 
-        {cloneElement(theme === 'light' ? <TbMoonStars /> : <TbSun className='' />,
+        {mounted && cloneElement(theme === 'light' ? <TbMoonStars /> : <TbSun className='' />,
           {
             onClick: () => setTheme(theme === 'dark' ? 'light' : 'dark'),
             style: { fontSize: '1.5rem' },
