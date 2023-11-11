3512d9de1787bf9b81b555c9888a32ed7bacbc27
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-11-10 22:54:24 -0800

    style: reduce the line height of the eco reward title

--- client/src/app/eco-rewards/page.tsx
+++ client/src/app/eco-rewards/page.tsx
@@ -5,7 +5,7 @@ import RewardsHistorySection from '@/components/eco-rewards/rewards-history-sect
 export default function EcoRewardsPage() {
   return (
     <div className='w-full flex flex-col gap-y-6'>
-      <h1 className='w-full text-left text-gray-0 font-bold'>Buy A Monthly Pass, Get A Day Pass</h1>
+      <h1 className='w-full text-left text-gray-0 font-bold leading-snug'>Buy A Monthly Pass, Get A Day Pass</h1>
       <p>When you buy 1 monthly pass, you can get 1 day free ride on any TransLink transportation by redeem your credit.</p>
       
       <CreditSection />
